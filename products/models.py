fcaf1f7744857a51140c0848f77b4db3e2d18584
Reuben Sinha <reuben.sinha@gmail.com>
2026-06-28 01:18:20 -0700

    feat: add Category, Tag, and Product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(
+        max_length=70, unique=True, blank=True
+    )  # May result in collisions is slugs are not unique, but for simplicity, we will assume they are unique.
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ["name"]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    slug = models.SlugField(max_length=30, unique=True, blank=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=120, unique=True, blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(
+        Category, on_delete=models.RESTRICT, related_name="products"
+    )  # Assume that a product belongs to a single category
+    tags = models.ManyToManyField(Tag, blank=True, related_name="products")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
