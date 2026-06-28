0dd48a643b6891072bb4009e8b84b5f51abd86fd
Reuben Sinha <reuben.sinha@gmail.com>
2026-06-28 02:31:15 -0700

    feat: add product list view with description, category and tag filters.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product, Category, Tag
+
+
+def product_list(request):
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+
+    query = request.GET.get("q", "")
+    category_id = request.GET.get("category", "")
+    tag_ids = request.GET.getlist("tags")  # getlist handles multiple tag selections
+
+    if query:
+        products = products.filter(description__icontains=query)
+    if category_id:
+        products = products.filter(category__id=category_id)
+    if tag_ids:
+        products = products.filter(tags__id__in=tag_ids).distinct()
+
+    context = {
+        "products": products,
+        "categories": categories,
+        "tags": tags,
+        "query": query,
+        "selected_category": category_id,
+        "selected_tags": tag_ids,
+    }
+    return render(request, "products/product_list.html", context)
